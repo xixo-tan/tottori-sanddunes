caacf7aafb28a141af34f406e8ed878fcd0f9ddd
xixo-tan <lilo93choco@icloud.com>
2022-10-06 02:26:44 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,40 +1,25 @@
 $(document).ready(function () {
-  var headerH = $("header").outerHeight(true);
-  function FixedAnime() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= headerH) {
-      $('header').addClass('HeightMin');
-    } else {
-      $('header').removeClass('HeightMin');
-    }
-  }
-  // 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function () {
-    FixedAnime();//スクロール途中からヘッダーの高さを変化させる関数を呼ぶ
-  });
-  // ページが読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function () {
-    FixedAnime();//スクロール途中からヘッダーの高さを変化させる関数を呼ぶ
-  });
 
   $('ul li a').click(function () {
     var headerVal = $("header").outerHeight(true);
     var scroll = $(window).scrollTop();
     var adjust = 0
-    if(scroll <= headerVal ){
+    if (scroll <= headerVal) {
       adjust = 70;
     }
     var elmHash = $(this).attr('href');
-    var pos = $(elmHash).offset().top-headerVal-adjust;
-    $('body,html').animate({scrollTop: pos}, 1000);
+    var pos = $(elmHash).offset().top - headerVal - adjust;
+    $('body,html').animate({
+      scrollTop: pos
+    }, 1000);
     return false;
   });
 
-  $('.fuwat').css('visibility','hidden');
-  $(window).scroll(function(){
+  $('.fuwat').css('visibility', 'hidden');
+  $(window).scroll(function () {
     var windowHeight = $(window).height(),
-    topWindow = $(window).scrollTop();
-    $('.fuwat').each(function(){
+      topWindow = $(window).scrollTop();
+    $('.fuwat').each(function () {
       var objectPosition = $(this).offset().top;
       if (topWindow > objectPosition - windowHeight + 200) {
         $(this).addClass("fuwatAnime");
@@ -42,15 +27,32 @@ $(document).ready(function () {
     });
   });
 
-    $('.slick').slick({
-      autoplay: true,
-      slidesToShow: 1,
-      infinite: true,
-      slidesToScroll: 1,
-      dots: true,
-      arrows: true,
-      centerPadding: '0',
-    });
+  $(".btn-gnavi").on("click", function () {
+    // ハンバーガーメニューの位置を設定
+    var rightVal = 0;
+    if ($(this).hasClass("open")) {
+      // 位置を移動させメニューを開いた状態にする
+      rightVal = -300;
+      // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+      $(this).removeClass("open");
+    } else {
+      // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+      $(this).addClass("open");
+    }
+    $("#global-navi").stop().animate({
+      right: rightVal
+    }, 200);
+  });
+
+  $('.slick').slick({
+    autoplay: true,
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    centerPadding: '0'
+  });
 
   $('.bg-switcher').bgSwitcher({
     images: [
@@ -59,6 +61,6 @@ $(document).ready(function () {
       '../images/sv-sunanooka.jpeg'
     ],
     interval: 4000,
-    loop: true
+    loop: true,
   })
 });
