e09e9621c65a52fb5ee483624eef9e149c45edbb
xixo-tan <lilo93choco@icloud.com>
2022-10-07 16:34:40 +0900

    humberger-nav editing

--- js/main.js
+++ js/main.js
@@ -27,22 +27,12 @@ $(document).ready(function () {
     });
   });
 
-  $(".btn-gnavi").on("click", function () {
-    // ハンバーガーメニューの位置を設定
-    var rightVal = 0;
-    if ($(this).hasClass("open")) {
-      // 位置を移動させメニューを開いた状態にする
-      rightVal = -300;
-      // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-      $(this).removeClass("open");
-    } else {
-      // メニューを開いたら次回クリック時は閉じた状態になるよう設定
-      $(this).addClass("open");
-    }
-    $("#global-navi").stop().animate({
-      right: rightVal
-    }, 200);
-  });
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+});
 
   $('.slick').slick({
     autoplay: true,
