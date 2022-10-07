7120d34dd79eee85ec99664b077579acb7a6e2a4
xixo-tan <lilo93choco@icloud.com>
2022-10-08 01:56:31 +0900

    add humberger-nav

--- js/main.js
+++ js/main.js
@@ -27,12 +27,12 @@ $(document).ready(function () {
     });
   });
 
-const ham = $('#js-hamburger');
-const nav = $('#js-nav');
-ham.on('click', function () { //ハンバーガーメニューをクリックしたら
-ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
-});
+  const ham = $('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
+  const nav = $('#js-nav'); //js-navの要素を取得し、変数navに格納
+  ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+    ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+    nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+  });
 
   $('.slick').slick({
     autoplay: true,
